0b3bbf5a052c6cf26d9a755826e45ed575db92f2
Xiangjun.Wu <s51043@edu.campus02.at>
2022-05-14 00:23:10 +0200

    Create tiles with position

--- core/src/main/java/at/compus02/swd/ss2022/game/tile_factory/TileFactory.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/tile_factory/TileFactory.java
@@ -4,10 +4,9 @@ package at.compus02.swd.ss2022.game.tile_factory;
 import at.compus02.swd.ss2022.game.gameobjects.*;
 import com.badlogic.gdx.utils.Array;
 
-import java.util.ArrayList;
-
 public class TileFactory
 {
+    GameObject gO;
     public GameObject getTile(String tileType)
     {
         switch(tileType)
@@ -27,9 +26,22 @@ public class TileFactory
 
     public Array<GameObject> createTiles(Array<GameObject> gameObject, String tileType, int amountTiles)
     {
+        float x = -240;
+        float y = 210;
         for (int i = 0; i < amountTiles; i++)
         {
-            gameObject.add(getTile(tileType));
+            if(x <= 240){
+                gO = getTile(tileType);
+                gO.setPosition(x,y);
+                gameObject.add(gO);
+                x=x+32;
+            }else{
+                x=-240;
+                if(y <= -210){
+                    return gameObject;
+                }
+                y=y-32;
+            }
         }
 
         return gameObject;
