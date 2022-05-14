073007a34748e7ba2cce3db3c7d62f8c2ff05b75
Lukas <lukas.la777@gmail.com>
2022-05-14 12:35:02 +0200

    added new Factory for Decoration

---
+++ core/src/main/java/at/compus02/swd/ss2022/game/factories/DecorationFactory.java
@@ -0,0 +1,20 @@
+package at.compus02.swd.ss2022.game.factories;
+
+import at.compus02.swd.ss2022.game.gameobjects.GameObject;
+import com.badlogic.gdx.utils.Array;
+
+public class DecorationFactory implements Factory
+{
+
+    @Override
+    public Array<GameObject> createGameObjects(Array<GameObject> gameObject, GameObjectType gameObjectType, int amountTiles)
+    {
+        return null;
+    }
+
+    @Override
+    public GameObject getObject(GameObjectType gameObjectType)
+    {
+        return null;
+    }
+}
