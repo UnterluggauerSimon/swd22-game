6aa3dac940f965502b6a1e250bbf1c70f7779f4e
Simon Unterluggauer <s.unterluggauer@navax.com>
2022-06-24 18:19:54 +0200

    run format function

--- core/src/main/java/at/compus02/swd/ss2022/game/converter/MapCalculator.java
+++ core/src/main/java/at/compus02/swd/ss2022/game/converter/MapCalculator.java
@@ -8,12 +8,12 @@ public class MapCalculator
 {
     public float arrayInitToMapPixel(int position)
     {
-       return (float) ((position - 8 )* 32);
+        return (float) ((position - 8) * 32);
     }
 
     public int mapPixelToArrayInt(float pixel)
     {
-            return Math.round((pixel + 255) / 32);
+        return Math.round((pixel + 255) / 32);
     }
 
     public boolean isMoveAllowed(GameObject[][] newMap, GameObject gameObject)
@@ -21,21 +21,24 @@ public class MapCalculator
         float newX = gameObject.getX();
         float newY = gameObject.getY();
 
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
         {
-            newX-= 4;
+            newX -= 4;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            newX+= 4;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+        {
+            newX += 4;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            newY+=4;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP))
+        {
+            newY += 4;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            newY-=4;
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
+        {
+            newY -= 4;
         }
 
-        if(newMap[mapPixelToArrayInt(newX)][mapPixelToArrayInt(newY-8)].isAllowedToWalk())
+        if (newMap[mapPixelToArrayInt(newX)][mapPixelToArrayInt(newY - 8)].isAllowedToWalk())
         {
             return true;
         }
@@ -47,7 +50,7 @@ public class MapCalculator
 
     public boolean isMoveAllowed(GameObject[][] newMap, float targetX, float targetY)
     {
-        if(newMap[mapPixelToArrayInt(targetX)][mapPixelToArrayInt(targetY-8)].isAllowedToWalk())
+        if (newMap[mapPixelToArrayInt(targetX)][mapPixelToArrayInt(targetY - 8)].isAllowedToWalk())
         {
             return true;
         }
